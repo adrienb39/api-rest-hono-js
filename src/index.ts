07d2093821d0fab1c845000a91ac6afccad0360f
adrienb39 <adrienb39@yahoo.com>
2025-03-25 06:38:55 +0100

    modification de la récupération des films et genres

--- src/index.ts
+++ src/index.ts
@@ -17,36 +17,36 @@ serve({
 })
 
 const dbConfig = {
-  host:process.env.DB_HOST,
-  port:process.env.DB_PORT,
-  user:process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  database:process.env.DB_NAME,
-  connectionLimit:10, // Le nombre maximal de connexion simultané
-  waitForConnections:true,
-  queueLimit:0,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10, // Le nombre maximal de connexion simultané
+  waitForConnections: true,
+  queueLimit: 0,
 }
 const pool = mysql.createPool(dbConfig)
 
 testConnexion()
 
-async function  testConnexion() {
-  try{
+async function testConnexion() {
+  try {
     // Demander une connexion au pool
-    const connexion=await pool.getConnection()
+    const connexion = await pool.getConnection()
     console.log("Connexion réussie")
     connexion.release()
-  }catch (error) {
+  } catch (error) {
     console.log("Bonsoir non")
   }
 }
 
 async function getFilms() {
-  try{
-    const [resultat] = await pool.query('SELECT * FROM films')
+  try {
+    const [resultat] = await pool.query('SELECT films.*, genres.nom AS genre_nom FROM films LEFT JOIN genres ON films.genre_id = genres.id')
     console.log(resultat) // Afficher dans la console la liste des films
-    return(resultat) // Retourne une promesse
-  }catch (error) {
+    return (resultat) // Retourne une promesse
+  } catch (error) {
     console.log('Erreur lors de la récupération des données')
     throw error // Retourner donc une promesse qui ne va pas être résolue
   }
@@ -69,7 +69,7 @@ app.use('*', async (c, next) => {
 app.get('/api/films', async (c) => {
   try {
     const films = await getFilms(); // Appel à votre fonction asynchrone
-    return c.json({status: 'success', data: films}); // Renvoi des données sous forme de JSON
+    return c.json({ status: 'success', data: films }); // Renvoi des données sous forme de JSON
   } catch (error) {
     console.error(error);
     return c.json({ message: 'Une erreur est survenue lors de la récupération des films.' }, 500);
